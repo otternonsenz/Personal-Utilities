67c26f7db950b15a01e008baa0adac21ae2e9bf5
otternonsenz <32229906+otternonsenz@users.noreply.github.com>
2017-10-30 10:33:07 -0700

    Create batch-exif-removal-tool.py

---
+++ batch-exif-removal-tool.py
@@ -0,0 +1,53 @@
+# Exif removal tool that clears all EXIF information for inputed files
+
+
+
+
+
+#Input Files
+
+
+
+
+
+#Scan Files for EXIF Data, check against common EXIF data fields
+
+
+
+
+
+#For any location/personal datalocate the field
+
+
+
+
+
+#Clear the field, Then save out cleaned file
+
+
+
+
+
+#Run 100 files at a time, thread processes for improved functionality
+
+
+
+
+
+#Do last check that all data is cleared
+
+
+
+
+
+#Choose Repository for "new" files to be output to
+
+
+
+
+
+#Close program
+
+
+
+
